462656ff68420175c542b4e4fb5f97bdf41eaa96
SarasaSarasa1 <1823010085@untels.edu.pe>
2021-07-28 23:25:36 -0500

    cambios pedido, etc

--- tienda/views/pedidos.py
+++ tienda/views/pedidos.py
@@ -1,30 +1,101 @@
+import datetime
+from functools import reduce
+from tienda.models import Articulo
+from tienda.models import Cliente
+from tienda.models import Pedido
+from tienda.models import Pedido_Articulo
 from django.shortcuts import render, redirect
 
 
 def carrito(request):
-    # TODO: cargar datos del carrito
-    # los datos estaran guardados en la sesion
     return render(request, 'tienda/pages/carrito.html')
 
 
 def agregar_articulo(request):
-    # TODO: Agregar articulo al carrito
-    # FIXME: Debe ser a la misma pagina de donde fue invocado
-    return redirect('/')
+    id_articulo = request.POST.get('id_articulo')
+    cantidad = int(request.POST.get('cantidad'))
+
+    try:
+        articulo = Articulo.objects.get(id_articulo = id_articulo)
+        if 'carrito' not in request.session:
+            request.session['carrito'] = []
+        if 'pedido' not in request.session:
+            request.session['pedido'] = {}
+        
+        request.session['carrito'].append({
+            'id_articulo': id_articulo,
+            'nombre': articulo.nombre,
+            'imagen': articulo.imagen,
+            'descripcion': articulo.descripcion,
+            'stock': articulo.stock,
+            'precio': articulo.precio,
+            'cantidad': cantidad,
+            'subtotal': articulo.precio * cantidad,
+            'pagina': F'/articulos/{articulo.id_articulo}'
+        })
+
+        request.session['pedido']['total'] = reduce(
+            lambda a, b: a['subtotal'] + b['subtotal'], 
+            request.session['carrito'])
+        request.session['pedido']['envio'] = 10.0
+        request.session['pedido']['cantidad_articulos'] = request.session['carrito']
+
+        request.session.modified = True
+    except Exception as e:
+        print(e)
+
+    return redirect("/")
 
 
 def verificar_pedido(request):
-    # TODO:
+    if 'autenticado' not in request.session:
+        return redirect('/')
+    if 'carrito' not in request.session:
+        return redirect('/')
+    if len(request.session['carrito']) == 0:
+        return redirect('/')
+
     return render(request, 'tienda/pages/verificar_pedido.html')
 
 
 def realizar_pedido(request):
-    # TODO: Realizar pedido
-    return render(request, 'tienda/pages/pedido_realizado.html')
+    if 'autenticado' not in request.session:
+        return redirect('/inicio-sesion')
+    if 'carrito' not in request.session:
+        return redirect('/')
+    if len(request.session['carrito']) == 0:
+        return redirect('/')
+    pedido = Pedido(
+        estado_pedido = 'HABILITADO',
+        num_comprob = '0123ABC',
+        total = request.session['pedido']['total'],
+        impuesto = request.session['pedido']['envio'],
+        serie_comprob = '1BC',
+        tipo_comprob = 'BOLETA',
+        fecha_pedido = datetime.datetime.now(),
+        fecha_entrega = datetime.datetime.now(),
+        id_cliente = Cliente.objects.get(id_cliente = request.session['cliente']['id_cliente'])
+    )
+    pedido.save()
+
+    for articulo in request.session['carrito']:
+        pedido_articulo = Pedido_Articulo(
+            id_pedido = Pedido.objects.get(id_pedido = pedido.id_pedido),
+            id_articulo = Articulo.objects.get(id_articulo = articulo['id_articulo']),
+            cantidad = articulo['cantidad']
+        )
+        pedido_articulo.save()
+
+    del request.session['carrito']
+    del request.session['pedido']
+    request.session.modified = True
+
+    return render(request, 'tienda/pages/pedido_realizado.html', { pedido: pedido })
 
 
 def listar_pedidos(request):
     if 'autenticado' not in request.session:
         return redirect('/')
-    # TODO: Buscar pedidos
-    return render(request, 'tienda/pages/pedidos.html')
+    id_cliente = request.session['cliente']['id_cliente']
+    pedidos = Pedido.objects.filter(id_cliente = id_cliente)
+    return render(request, 'tienda/pages/pedidos.html', { 'pedidos': pedidos })
