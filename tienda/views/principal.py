f7c1ce77c9d73c06c0667eba50202d33fa8bf984
Jibaru <ignacioruedaboada@hotmail.com>
2021-08-02 00:20:14 -0500

    Ordenado

--- tienda/views/principal.py
+++ tienda/views/principal.py
@@ -2,16 +2,11 @@ from django.shortcuts import render
 from tienda.models import *
 
 def inicio(request):
-    
-    # ODOT: Realizar busqueda de todos los articulos
     articulos= Articulo.objects.all()
-    
     contexto = {
-  
         'articulos': articulos
     }
     return render(request, 'tienda/pages/inicio.html', contexto)
-    
 
 
 def informacion(request):
